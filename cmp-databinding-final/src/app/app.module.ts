c0ee9c3a9ba242a2d183050a493752848eb9dbb0
Ravi Gupta <ravigupta112>
2019-02-28 22:52:58 +0530

    Pass value from on component to another componenet

--- cmp-databinding-final/src/app/app.module.ts
+++ cmp-databinding-final/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ServerElementComponent } from './server-element/server-element.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
